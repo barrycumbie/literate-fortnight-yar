19ce0dd4799eaa2442b5fa107eb3f211fb9b05b2
Cumbie <bcumbie@CSM2-0244-MFXXH.main.local.una.edu>
2026-02-12 14:25:06 -0600

    init code

---
+++ app.mjs
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Barry says hello from: <a href="https://github.com/barrycumbie/literate-fortnight-yar" target="blank">literate fortnight yar</a>')
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
